0a5c7fc028bf390e76aa3c11e33bdd2e2e202049
aneirfan <bolosaholic@gmail.com>
2021-07-06 15:17:09 +0700

    pasang postgres db

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -16,12 +16,12 @@ class NotesHandler {
     this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
   }
 
-  postNoteHandler(request, h) {
+  async postNoteHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload);
       const { title = 'untitled', body, tags } = request.payload;
 
-      const noteId = this._service.addNote({ title, body, tags });
+      const noteId = await this._service.addNote({ title, body, tags });
 
       const response = h.response({
         status: 'success',
@@ -53,8 +53,8 @@ class NotesHandler {
     }
   }
 
-  getNotesHandler() {
-    const notes = this._service.getNotes();
+  async getNotesHandler() {
+    const notes = await this._service.getNotes();
     return {
       status: 'success',
       data: {
@@ -63,26 +63,17 @@ class NotesHandler {
     };
   }
 
-  getNoteByIdHandler(request, h) {
+  async getNoteByIdHandler(request, h) {
     try {
       const { id } = request.params;
-      const note = this._service.getNoteById(id);
+      const note = await this._service.getNoteById(id);
       return {
         status: 'success',
         data: {
           note,
         },
       };
-    }
-    // catch (error) {
-    //   const response = h.response({
-    //     status: 'fail',
-    //     message: error.message,
-    //   });
-    //   response.code(404);
-    //   return response;
-    // }
-    catch (error) {
+    } catch (error) {
       if (error instanceof ClientError) {
         const response = h.response({
           status: 'fail',
@@ -103,27 +94,19 @@ class NotesHandler {
     }
   }
 
-  putNoteByIdHandler(request, h) {
+  async putNoteByIdHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload);
+      const { title, body, tags } = request.payload;
       const { id } = request.params;
 
-      this._service.editNoteById(id, request.payload);
+      await this._service.editNoteById(id, { title, body, tags });
 
       return {
         status: 'success',
         message: 'Catatan berhasil diperbarui',
       };
-    }
-    // catch (error) {
-    //   const response = h.response({
-    //     status: 'fail',
-    //     message: error.message,
-    //   });
-    //   response.code(404);
-    //   return response;
-    // }
-    catch (error) {
+    } catch (error) {
       if (error instanceof ClientError) {
         const response = h.response({
           status: 'fail',
@@ -144,24 +127,16 @@ class NotesHandler {
     }
   }
 
-  deleteNoteByIdHandler(request, h) {
+  async deleteNoteByIdHandler(request, h) {
     try {
       const { id } = request.params;
-      this._service.deleteNoteById(id);
+      await this._service.deleteNoteById(id);
+
       return {
         status: 'success',
         message: 'Catatan berhasil dihapus',
       };
-    }
-    // catch (error) {
-    //   const response = h.response({
-    //     status: 'fail',
-    //     message: 'Catatan gagal dihapus. Id tidak ditemukan',
-    //   });
-    //   response.code(404);
-    //   return response;
-    // }
-    catch (error) {
+    } catch (error) {
       if (error instanceof ClientError) {
         const response = h.response({
           status: 'fail',
